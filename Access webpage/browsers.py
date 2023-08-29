986dfa2b74612981271bf5116de45e29ae54b8c5
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-29 19:42:47 +0600

    Modified browsers.py

--- Access webpage/browsers.py
+++ Access webpage/browsers.py
@@ -3,46 +3,49 @@ class Browser:
         self.url = url
         self.api = API(self.url)
         self.response = None
- 
- 
+
     def request(self, http_method: str, endpoint: str):
-          self.response = self.api.request(http_method, endpoint)
+        self.response = request(http_method, endpoint)
  
     def find_object(self, object_name: str):
         if object_name in self.response:
             return self.response[object_name]
         return None
- 
+
+
 mock_actions = "Mock value for actions"
- 
+
+
+def request(http_method: str, endpoint: str):
+    if http_method == 'GET' and endpoint == "/api/v1/users":
+        # Simulate making API request and handling response
+        response = {
+            "data": [
+                {"last_name": "Admin", "first_name": "Mr", "email": "admin@uniq.app", "confirmed": True,
+                 "role": "Administrator", "other_permissions": "N/A", "social": "None", "last_updated": 8,
+                 "actions": mock_actions},
+                {"last_name": "User", "first_name": "Teacher", "email": "teacher@uniq.app", "confirmed": True,
+                 "role": "Teacher", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+                 "actions": mock_actions},
+                {"last_name": "User", "first_name": "Student", "email": "student@uniq.app", "confirmed": True,
+                 "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+                 "actions": mock_actions},
+                {"last_name": "User", "first_name": "Normal", "email": "user@uniq.app", "confirmed": True,
+                 "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+                 "actions": mock_actions}
+            ],
+            "Meta": {"total": 4, "limit": 10, "offset": 0}
+        }
+        return response
+    else:
+        return None
+
+
 class API:
     def __init__(self, url: str):
         self.url = url
- 
-    def request(self, http_method: str, endpoint: str):
-        if http_method == 'GET' and endpoint == "/api/v1/users":
-            # Simulate making API request and handling response
-            response = {
-                "data": [
-                    {"last_name": "Admin", "first_name": "Mr", "email": "admin@uniq.app", "confirmed": True, 
-                     "role": "Administrator", "other_permissions": "N/A", "social": "None", "last_updated": 8,
-					 "actions": mock_actions},
-                    {"last_name": "User", "first_name": "Teacher", "email": "teacher@uniq.app", "confirmed": True, 
-                     "role": "Teacher", "other_permissions": "N/A", "social": "None", "last_updated": 4,
-					 "actions": mock_actions},
-                    {"last_name": "User", "first_name": "Student", "email": "student@uniq.app", "confirmed": True, 
-                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
-					 "actions": mock_actions},
-                    {"last_name": "User", "first_name": "Normal", "email": "user@uniq.app", "confirmed": True, 
-                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
-					 "actions": mock_actions}
-                ],
-                "Meta": {"total": 4, "limit": 10, "offset": 0}
-            }
-            return response
-        else:
-            return None
- 
+
+
 # Creating instances of the classes
 website_url = "https://myweb.com"
 api_url = "https://api.myweb.com"
