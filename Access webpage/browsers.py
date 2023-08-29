26cef416c0fdd14bcafa078706d67549babb155a
Md. Khalid Hussain <hussainkhalid70@yahoo.com>
2023-08-29 19:30:38 +0600

    modified browsers.py

--- Access webpage/browsers.py
+++ Access webpage/browsers.py
@@ -1,47 +1,55 @@
 class Browser:
     def __init__(self, url: str):
         self.url = url
-    
+        self.api = API(self.url)
+        self.response = None
+ 
+ 
+    def request(self, http_method: str, endpoint: str):
+          self.response = self.api.request(http_method, endpoint)
+ 
     def find_object(self, object_name: str):
-        if object_name == "user_list":
-            # Simulate interacting with the user list
-            print(f"Interacting with user list on {self.url}")
-        else:
-            print(f"Object '{object_name}' not found on {self.url}")
-
+        if object_name in self.response:
+            return self.response[object_name]
+        return None
+ 
+mock_actions = "Mock value for actions"
+ 
 class API:
     def __init__(self, url: str):
         self.url = url
-    
+ 
     def request(self, http_method: str, endpoint: str):
-        if endpoint == "/api/v1/users":
+        if http_method == 'GET' and endpoint == "/api/v1/users":
             # Simulate making API request and handling response
             response = {
                 "data": [
                     {"last_name": "Admin", "first_name": "Mr", "email": "admin@uniq.app", "confirmed": True, 
-                     "role": "Administrator", "other_permissions": "N/A", "social": "None", "last_updated": 8},
+                     "role": "Administrator", "other_permissions": "N/A", "social": "None", "last_updated": 8,
+					 "actions": mock_actions},
                     {"last_name": "User", "first_name": "Teacher", "email": "teacher@uniq.app", "confirmed": True, 
-                     "role": "Teacher", "other_permissions": "N/A", "social": "None", "last_updated": 4},
+                     "role": "Teacher", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+					 "actions": mock_actions},
                     {"last_name": "User", "first_name": "Student", "email": "student@uniq.app", "confirmed": True, 
-                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4},
+                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+					 "actions": mock_actions},
                     {"last_name": "User", "first_name": "Normal", "email": "user@uniq.app", "confirmed": True, 
-                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4}
+                     "role": "Student", "other_permissions": "N/A", "social": "None", "last_updated": 4,
+					 "actions": mock_actions}
                 ],
                 "Meta": {"total": 4, "limit": 10, "offset": 0}
             }
-            print(f"Making a {http_method} request to API endpoint '{endpoint}' at {self.url}")
-            print("API Response:", response)
+            return response
         else:
-            print(f"API endpoint '{endpoint}' not found at {self.url}")
-
+            return None
+ 
 # Creating instances of the classes
 website_url = "https://myweb.com"
 api_url = "https://api.myweb.com"
-
-browser = Browser(website_url)
-api = API(api_url)
-
+ 
+browser = Browser(api_url)
+browser.request("GET", "/api/v1/users")
 # Using the instances to perform actions
-browser.find_object("user_list")
-api.request("GET", "/api/v1/users")
+user_list = browser.find_object("data")
 
+print(user_list)
